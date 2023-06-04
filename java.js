779dcab8a40c11c13002e76f76c1082273083abc
mirandariva92 <129214459+mirandariva92@users.noreply.github.com>
2023-06-04 11:58:29 +0200

    Esercitazioni Javascript Maggio - Giugno 2023

---
+++ java.js
@@ -0,0 +1,132 @@
+// Esercitazione 1 - 09 maggio 2023
+/* Esercizio 1
+let movieTitle = 'Pulp Fiction';
+let movieYear = '1994';
+let movieDirector = 'Quentin Tarantino';
+
+console.log (`movie:${ movieTitle} \n year: ${movieYear}\n director ${movieDirector}`);
+console.log (`${ movieTitle} (${movieYear} ${movieDirector})`);
+*/
+
+/* Esercizio 2
+let bookTitle
+bookTitle ='Piccole Donne';
+let bookDescription = 'Libro su giovani donne che vogliono emanciparsi in una società patriarcale.';
+
+console.log (`${ bookTitle} \n ${bookDescription}`);
+*/
+
+/* Esercizio 3
+let name;
+name= 'John';
+let admin;
+admin = name;
+alert (admin);
+*/
+
+/* Esercizio 4
+let username = prompt ('inserisci il tuo user');
+let password = prompt ('inserisci la password');
+
+alert(`Benvenuto ${username} in area personale`);
+*/
+
+/* Esercizio 5
+let name = prompt ('inserisci il nome');
+let surname = prompt ('inserisci il cognome');
+let age = prompt ('inserisci eta');
+
+console.log (`${ name} \n ${surname} \n ${age}`);
+*/
+
+// Esercitazione 2 - 11 maggio 2023
+
+/* Esercizio 1
+let x= 10;
+let y= 20;
+let a= 35;
+let b= 50;
+let c= 70;
+
+sum = (x + y + a + b + c);
+modulo = (x % y % a % b % c)
+console.log (`${sum} ${modulo}`);
+*/
+
+/*Esercizione 2
+let x= parseInt(prompt ());
+let y= parseInt(prompt ());
+let a= parseInt(prompt ());
+let b= parseInt(prompt ());
+let c= parseInt(prompt ());
+let sum;
+let media;
+
+sum = ((x)+(y)+(a)+(b)+(c));
+media = (sum / 5);
+console.log (`${sum} ${media}`);
+*/
+
+/*Esercizio 3
+let tempBari= 23;
+let tempNaples= 28;
+let tempRome= 29; 
+let tempMilan= 19;
+
+console.log (`la temperatura più alta è ${Math.max((tempBari), (tempNaples),(tempRome),(tempMilan))} e la temperatura più basse è ${Math.min((tempBari), (tempNaples),(tempRome),(tempMilan))}`);
+*/
+
+/* Esercizio 4
+let annoAttuale = 2023;
+let annoNascita = 1996;
+
+age = ((annoAttuale) - (annoNascita));
+result = (100 - (age));
+alert (`età della persona è ${(age)} e mancano ${(result)} per raggiungere i 100`);
+console.log (`età della persona è ${(age)} e mancano ${(result)} per raggiungere i 100`);
+*/
+
+/* Esercizio 5
+let utente1 = prompt('scrivi il tuo nome');
+let utente2 = prompt('scrivi il tuo nome');
+
+let result = utente1 == utente2;
+console.log ((result));
+*/
+
+/* Esercizio 6 non compreso */
+
+/* Esercizio 7 da capire
+
+const totalCats = 46;
+const catPorRow = 5;
+
+let rows = Math.ceil (totalCats / catPorRow);
+let missingCats = catPorRow - (totalCats % catPorRow);
+
+console.log (`Il numero totale delle file è ${(rows)}`);
+console.log (`il numero di gatti mancanti per completare una nuova fila è ${(missingCats)}`);
+*/
+
+
+
+/* Esericizio 8 da capire
+let min = 1;
+let max = 6;
+let n= Math.floor(Math.random() * (max - min + 1)) + min;
+
+console.log (((n)));
+*/
+
+
+
+
+
+
+// Esercitazione 3 - Weekend 13 maggio 2023
+// Esercitazione 5 - 18 maggio 2023
+// Esercitazione 6 - Weekend 19 Maggio 2023
+// Esercitazione 7 - 23 maggio 2023
+// Esercitazione 8 - 25 maggio 2023
+// Esercitazione 9 - 30 maggio 2023
+
